65d596422fe61e202ad2b51c4b8df11385464e21
kings10-hiroki <h61147366@gmail.com>
2020-05-26 16:50:40 +0900

    first commit

---
+++ app/views/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo SITENAME; ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/style.css">
+</head>
+
+<body>
